a3416634096b09f71f89bba24d6c000bd25128ef
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2024-08-07 18:33:14 +0300

    tree class ready

--- red-black-tree.py
+++ red-black-tree.py
@@ -22,7 +22,7 @@ class Node:
                     print("L----", end="")
                     indent += "|    "
                 color = "RED" if not node.color else "BLACK"
-                print(f"{node.key}({color}, bh={node.black_height})")
+                print(f"{node.key}({color})")
                 print_helper(node.left, indent, False)
                 print_helper(node.right, indent, True)
 
@@ -46,15 +46,16 @@ class RedBlackTree:
     def black_height(self, node):
         return 0 if node is None else node.black_height
 
-    def rotate_left(self, tree):
-        new_tree = tree.right
-        tree.right = new_tree.left
-        new_tree.left = tree
-        new_tree.color = tree.color
-        tree.color = False
-        new_tree.black_height = tree.black_height
-        tree.black_height -= 1
-        return new_tree
+    def rotate_left(self, node):
+        right_tree = node.right
+        node.right = right_tree.left
+
+        right_tree.left = node
+        right_tree.color = node.color
+        node.color = False
+        right_tree.black_height = node.black_height
+        node.black_height -= 1
+        return right_tree
 
     def rotate_right(self, tree):
         new_tree = tree.left
@@ -116,7 +117,7 @@ class RedBlackTree:
                 if tree.right is None:
                     return tree.left
 
-                left, last_key = split_last(tree.left)
+                left, last_key = self.split_last(tree.left)
                 tree.key = last_key
                 tree.left = left
 
@@ -126,31 +127,31 @@ class RedBlackTree:
         if self.root:
             self.root.color = True
 
-    def join_right_rb(self, left_tree, key, right_tree):
+    def join_right(self, left_tree, key, right_tree):
         if self.black_height(left_tree) < self.black_height(right_tree):
-            right_tree.left = self.join_right_rb(left_tree, key, right_tree.left)
+            right_tree.left = self.join_right(left_tree, key, right_tree.left)
             return self.fix_up(right_tree)
         elif self.black_height(left_tree) > self.black_height(right_tree):
-            left_tree.right = self.join_right_rb(left_tree.right, key, right_tree)
+            left_tree.right = self.join_right(left_tree.right, key, right_tree)
             return self.fix_up(left_tree)
         else:
             return Node(key, False, left_tree, right_tree, self.black_height(left_tree) + 1)
 
-    def join_left_rb(self, left_tree, key, right_tree):
+    def join_left(self, left_tree, key, right_tree):
         if self.black_height(left_tree) < self.black_height(right_tree):
-            right_tree.left = self.join_left_rb(left_tree, key, right_tree.left)
+            right_tree.left = self.join_left(left_tree, key, right_tree.left)
             return self.fix_up(right_tree)
         elif self.black_height(left_tree) > self.black_height(right_tree):
-            left_tree.right = self.join_left_rb(left_tree.right, key, right_tree)
+            left_tree.right = self.join_left(left_tree.right, key, right_tree)
             return self.fix_up(left_tree)
         else:
             return Node(key, False, left_tree, right_tree, self.black_height(left_tree) + 1)
 
-    def join_rb(self, left_tree, key, right_tree):
+    def join(self, left_tree, key, right_tree):
         if self.black_height(left_tree) <= self.black_height(right_tree):
-            return self.join_right_rb(left_tree, key, right_tree)
+            return self.join_right(left_tree, key, right_tree)
         else:
-            return self.join_left_rb(left_tree, key, right_tree)
+            return self.join_left(left_tree, key, right_tree)
 
     def join_with_key(self, left_tree, key, right_tree):
         if key is None:
@@ -158,9 +159,9 @@ class RedBlackTree:
                 return right_tree
             if right_tree is None:
                 return left_tree
-            return self.join_rb(left_tree, right_tree.key, right_tree.right)
+            return self.join(left_tree, right_tree.key, right_tree.right)
         else:
-            return self.join_rb(left_tree, key, right_tree)
+            return self.join(left_tree, key, right_tree)
 
     def split(self, tree, key):
         if tree is None:
@@ -168,10 +169,10 @@ class RedBlackTree:
 
         if key < tree.key:
             left, found, right = self.split(tree.left, key)
-            return left, found, self.join_rb(right, tree.key, tree.right)
+            return left, found, self.join(right, tree.key, tree.right)
         elif key > tree.key:
             left, found, right = self.split(tree.right, key)
-            return self.join_rb(tree.left, tree.key, left), found, right
+            return self.join(tree.left, tree.key, left), found, right
         else:
             return tree.left, True, tree.right
 
@@ -180,54 +181,62 @@ class RedBlackTree:
             return tree.left, tree.key
         else:
             left, last_key = self.split_last(tree.right)
-            return self.join_rb(tree.left, tree.key, left), last_key
+            return self.join(tree.left, tree.key, left), last_key
 
     def union(self, tree1, tree2):
-        if tree1 is None:
-            return tree2
-        if tree2 is None:
-            return tree1
+        def union_helper(tree1, tree2):
+            if tree1 is None:
+                return tree2
+            if tree2 is None:
+                return tree1
 
-        left1, in_tree1, right1 = self.split(tree1, tree2.key)
-        left2, in_tree2, right2 = self.split(tree2, tree2.key)
+            left1, in_tree1, right1 = self.split(tree1, tree2.key)
+            left2, in_tree2, right2 = self.split(tree2, tree2.key)
 
-        merged_left = self.union(left1, left2)
-        merged_right = self.union(right1, right2)
-        return self.join_with_key(merged_left, tree2.key if in_tree1 or in_tree2 else None, merged_right)
+            merged_left = union_helper(left1, left2)
+            merged_right = union_helper(right1, right2)
+            return self.join_with_key(merged_left, tree2.key if in_tree1 or in_tree2 else None, merged_right)
+        return RedBlackTree(union_helper(tree1.root, tree2.root))
+
 
     def intersection(self, tree1, tree2):
-        if tree1 is None or tree2 is None:
-            return None
+        def intersection_helper(tree1, tree2):
+            if tree1 is None or tree2 is None:
+                return None
 
-        left1, in_tree1, right1 = self.split(tree1, tree2.key)
-        left2, in_tree2, right2 = self.split(tree2, tree2.key)
+            left1, in_tree1, right1 = self.split(tree1, tree2.key)
+            left2, in_tree2, right2 = self.split(tree2, tree2.key)
 
-        if in_tree1 and in_tree2:
-            intersected_left = self.intersection(left1, left2)
-            intersected_right = self.intersection(right1, right2)
-            return self.join_with_key(intersected_left, tree2.key, intersected_right)
-        else:
-            intersected_left = self.intersection(left1, left2)
-            intersected_right = self.intersection(right1, right2)
-            return self.join_with_key(intersected_left, None, intersected_right)
+            if in_tree1 and in_tree2:
+                intersected_left = intersection_helper(left1, left2)
+                intersected_right = intersection_helper(right1, right2)
+                return self.join_with_key(intersected_left, tree2.key, intersected_right)
+            else:
+                intersected_left = intersection_helper(left1, left2)
+                intersected_right = intersection_helper(right1, right2)
+                return self.join_with_key(intersected_left, None, intersected_right)
+        return RedBlackTree(intersection_helper(tree1.root, tree2.root))
 
     def set_difference(self, tree1, tree2):
-        if tree1 is None:
-            return None
-        if tree2 is None:
-            return tree1
+        def set_difference_helper(tree1, tree2):
+            if tree1 is None:
+                return None
+            if tree2 is None:
+                return tree1
 
-        left1, in_tree1, right1 = self.split(tree1, tree2.key)
-        left2, in_tree2, right2 = self.split(tree2, tree2.key)
+            left1, in_tree1, right1 = self.split(tree1, tree2.key)
+            left2, in_tree2, right2 = self.split(tree2, tree2.key)
 
-        if in_tree2:
-            diff_left = self.set_difference(left1, left2)
-            diff_right = self.set_difference(right1, right2)
-            return self.join_with_key(diff_left, None, diff_right)
-        else:
-            diff_left = self.set_difference(left1, left2)
-            diff_right = self.set_difference(right1, right2)
-            return self.join_with_key(diff_left, tree1.key, diff_right)
+            if in_tree2:
+                diff_left = set_difference_helper(left1, left2)
+                diff_right = set_difference_helper(right1, right2)
+                return self.join_with_key(diff_left, None, diff_right)
+            else:
+                diff_left = set_difference_helper(left1, left2)
+                diff_right = set_difference_helper(right1, right2)
+                return self.join_with_key(diff_left, tree1.key, diff_right)
+        return RedBlackTree(set_difference_helper(tree1.root, tree2.root))
+
 
     def print_tree(self):
         self.root.print_subtree()
@@ -236,41 +245,43 @@ class RedBlackTree:
 
 
 
-# Create an instance of RedBlackTree
-rbt = RedBlackTree()
+def test():
+    # Create an instance of RedBlackTree
+    rbt = RedBlackTree()
 
-# Insert elements into the red-black tree
-keys = [20, 15, 25, 10, 5, 1]
-for key in keys:
-    rbt.insert(key)
-    # rbt.print_tree()
-    # print("********************************************",key)
+    # Insert elements into the red-black tree
+    keys = [20, 15, 25, 10, 5, 1]
+    for key in keys:
+        rbt.insert(key)
+        # rbt.print_tree()
+        # print("********************************************",key)
 
-# Print the red-black tree
-print("Red-Black Tree:")
-rbt.print_tree()
+    # Print the red-black tree
+    print("Red-Black Tree:")
+    rbt.print_tree()
 
-# Perform union, intersection, and set difference operations
-rbt2 = RedBlackTree()
-keys2 = [30, 25, 40, 35]
-for key in keys2:
-    rbt2.insert(key)
+    # Perform union, intersection, and set difference operations
+    rbt2 = RedBlackTree()
+    keys2 = [30, 25, 40, 35]
+    for key in keys2:
+        rbt2.insert(key)
 
-print("\nRed-Black Tree 2:")
-rbt2.print_tree()
+    print("\nRed-Black Tree 2:")
+    rbt2.print_tree()
 
-print("\nUnion of Tree 1 and Tree 2:")
-union_tree = rbt.union(rbt.root, rbt2.root)
-# rbt.print_tree(union_tree)
-union_tree.print_tree()
+    print("\nUnion of Tree 1 and Tree 2:")
+    union_tree = rbt.union(rbt, rbt2)
+    # rbt.print_tree(union_tree)
+    union_tree.print_tree()
 
-print("\nIntersection of Tree 1 and Tree 2:")
-intersection_tree = rbt.intersection(rbt.root, rbt2.root)
-rbt.print_tree(intersection_tree)
+    print("\nIntersection of Tree 1 and Tree 2:")
+    intersection_tree = rbt.intersection(rbt, rbt2)
+    intersection_tree.print_tree()
 
-print("\nSet Difference of Tree 1 and Tree 2:")
-difference_tree = rbt.set_difference(rbt.root, rbt2.root)
-rbt.print_tree(difference_tree)
+    print("\nSet Difference of Tree 1 and Tree 2:")
+    difference_tree = rbt.set_difference(rbt, rbt2)
+    difference_tree.print_tree()
 
+test()
 
 
